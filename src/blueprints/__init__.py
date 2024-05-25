36f8586fe7e003929f723378195987e6053c9ca1
Pavel <pbkrutov@gmail.com>
2024-05-25 18:01:57 +0300

    finish quest create route

--- src/blueprints/__init__.py
+++ src/blueprints/__init__.py
@@ -2,8 +2,8 @@ from sanic import Blueprint
 from src.blueprints.users import user_blueprint
 # from src.blueprints.events import event_blueprint_group
 # from src.blueprints.cms import content_blueprint
-# from src.blueprints.quests import quest_blueprint
+from src.blueprints.quests import quest_blueprint
 from src.blueprints.projects import project_blueprint
 
-blueprints = [user_blueprint, project_blueprint]
+blueprints = [user_blueprint, project_blueprint, quest_blueprint]
 blueprint_group = Blueprint.group(url_prefix='/', *blueprints, version=0.1, version_prefix='/api/v')
--- src/blueprints/quests/__init__.py
+++ src/blueprints/quests/__init__.py
@@ -1,25 +1,27 @@
 from sanic import Blueprint, Request
-from sanic import json as sanicjson
+import sanic
+from sanic_ext import openapi
+
+from src.database import Database
+from src.models import quest
+from src.views.quest import QuestView, QuestCreateView
 
 quest_blueprint = Blueprint("quest_routes", url_prefix='/quests')
 
 
 @quest_blueprint.route('/', methods=['POST'])
-async def quests(request: Request):
-    if request.method == 'GET':
-        ...
-    elif request.method == 'POST':
-        ...
+@openapi.body(content={'application/json': QuestCreateView.view_schema()})
+@openapi.response(status=201, description="Success")
+async def create_quest(request: Request, db: Database):
+    view = QuestCreateView(**request.json)
+
+    await QuestView.new(db, view)
+
+    return sanic.HTTPResponse(status=201)
 
 
 @quest_blueprint.get('/<quest_id:int>')
-async def get_quest(request: Request, quest_id: int):
-    ...
-
-
-async def get_all_quests(request: Request):
-    ...
-
-
-async def create_quest(request: Request):
-    ...
+@openapi.response(status=200,
+                  content={'application/json': QuestView.view_schema()})
+async def get_quest(request: Request, db: Database, quest_id: int):
+    quest_view = await QuestView.build(db, quest_id)
