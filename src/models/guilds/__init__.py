e8be12edc4f97ca872dc51815c795bb4166b3a2b
Pavel <pbkrutov@gmail.com>
2024-06-24 22:43:46 +0300

    import all models into init.py

---
+++ src/models/guilds/__init__.py
@@ -0,0 +1,5 @@
+from .channels import ChannelModel
+from .features import FeatureModel
+from .guild import GuildModel
+from .leaderboards import LeaderboardModel
+from .playtime import GuildPlaytimeAnalysis, OnlineUsersModel
