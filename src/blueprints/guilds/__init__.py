a01b4edbc5dc45911d96e7afefb5de7f578c9349
Pavel <pbkrutov@gmail.com>
2024-07-02 16:03:23 +0300

    add new leaderboard routes

--- src/blueprints/guilds/__init__.py
+++ src/blueprints/guilds/__init__.py
@@ -64,7 +64,7 @@ async def get_guild_playtime(request: Request, db: Database, guild_id: int):
 
     FOR NOW IT IS NOT FULLY COMPLETE
     """
-    guild_analysis = await guild.GuildPlaytimeAnalysis.fetch(db, guild_id)
+    guild_analysis = await guilds.GuildPlaytimeAnalysis.fetch(db, guild_id)
 
     return sanic.json(guild_analysis.model_dump(), default=str)
 
@@ -80,7 +80,55 @@ async def get_playtime_leaderboard(request: Request, db: Database, guild_id: int
 
     Returns the guild's playtime leaderboard, in order. Playtime is in seconds.
     """
-    guild_leaderboard = await guild.LeaderboardModel.get_playtime(db, month, guild_id)
+    guild_leaderboard = await guilds.LeaderboardModel.fetch_playtime(db, month, guild_id)
+
+    return sanic.json(guild_leaderboard.model_dump(), default=str)
+
+
+@guild_blueprint.route('/<guild_id:int>/leaderboard/currency', methods=['GET'])
+@openapi.response(status=200,
+                  content={'application/json': guilds.LeaderboardModel.doc_schema()},
+                  description='Success')
+@openapi.response(status=404, description='Error')
+async def get_currency_leaderboard(request: Request, db: Database, guild_id: int):
+    """
+    Get Guild Currency Leaderboard
+
+    Returns the guild's currency leaderboard, in order.
+    """
+    guild_leaderboard = await guilds.LeaderboardModel.fetch_currency(db, guild_id)
+
+    return sanic.json(guild_leaderboard.model_dump(), default=str)
+
+
+@guild_blueprint.route('/<guild_id:int>/leaderboard/levels', methods=['GET'])
+@openapi.response(status=200,
+                  content={'application/json': guilds.LeaderboardModel.doc_schema()},
+                  description='Success')
+@openapi.response(status=404, description='Error')
+async def get_levels_leaderboard(request: Request, db: Database, guild_id: int):
+    """
+    Get Guild Levels Leaderboard
+
+    Returns the guild's playtime leaderboard, in order. Playtime is in seconds.
+    """
+    guild_leaderboard = await guilds.LeaderboardModel.fetch_levels(db, guild_id)
+
+    return sanic.json(guild_leaderboard.model_dump(), default=str)
+
+
+@guild_blueprint.route('/<guild_id:int>/leaderboard/quests', methods=['GET'])
+@openapi.response(status=200,
+                  content={'application/json': guilds.LeaderboardModel.doc_schema()},
+                  description='Success')
+@openapi.response(status=404, description='Error')
+async def get_quests_leaderboard(request: Request, db: Database, guild_id: int):
+    """
+    Get Guild Quests Leaderboard
+
+    Returns the guild's playtime leaderboard, in order. Playtime is in seconds.
+    """
+    guild_leaderboard = await guilds.LeaderboardModel.fetch_quests(db, guild_id)
 
     return sanic.json(guild_leaderboard.model_dump(), default=str)
 
@@ -97,6 +145,6 @@ async def get_online_users(request: Request, db: Database, guild_id: int):
     Get a list of ThornyIDs that are currently
     online on the server, along with their session time.
     """
-    online = await guild.OnlineUsersSummary.build(db, guild_id)
+    online = await guilds.OnlineUsersModel.fetch(db, guild_id)
 
     return sanic.json(online.model_dump(), default=str)
