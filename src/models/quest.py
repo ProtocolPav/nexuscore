7dd9db296687ba869a23110563b95de207779865
Pavel <pbkrutov@gmail.com>
2024-11-06 17:06:01 +0200

    add new objective display and support for the encounter type

--- src/models/quest.py
+++ src/models/quest.py
@@ -60,9 +60,10 @@ class ObjectiveModel(BaseModel):
     quest_id: int
     description: str
     objective: InteractionRef
+    display: Optional[str]
     order: int
     objective_count: int
-    objective_type: Literal["kill", "mine"]
+    objective_type: Literal["kill", "mine", "encounter"]
     natural_block: bool
     objective_timer: Optional[float]
     required_mainhand: Optional[InteractionRef]
@@ -76,6 +77,7 @@ class ObjectiveModel(BaseModel):
                                               quest_id,
                                               description,
                                               objective,
+                                              display,
                                               "order",
                                               objective_count,
                                               objective_type,
