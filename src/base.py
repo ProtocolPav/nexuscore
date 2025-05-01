6fbe87a756bccd3136a6fac5cb8b01dc0f8b17ca
Pavel <pbkrutov@gmail.com>
2025-05-01 23:11:06 +0300

    add new base classes and methods to be used across all models. Following the DRY Principles

---
+++ src/base.py
@@ -0,0 +1,51 @@
+import pydantic
+from typing import Generic, TypeVar, List, Optional, get_type_hints
+
+from src.database import Database
+
+
+class BaseModel(pydantic.BaseModel):
+    @classmethod
+    def doc_schema(cls):
+        return cls.model_json_schema(ref_template="#/components/schemas/{model}")
+
+    @classmethod
+    async def fetch(cls, db: Database, *args, **kwargs):
+        raise NotImplementedError("fetch() must be implemented in subclasses")
+
+    @classmethod
+    async def create(cls, db: Database, *args, **kwargs):
+        raise NotImplementedError("create() must be implemented in subclasses")
+
+    async def update(self, db: Database):
+        raise NotImplementedError("update() must be implemented in subclasses")
+
+
+def optional_model(name: str, base: type[BaseModel]) -> type[BaseModel]:
+    annotations = get_type_hints(base)
+    optional_fields = {
+        k: (Optional[v], None)
+        for k, v in annotations.items()
+    }
+    return pydantic.create_model(name, **optional_fields, __base__=BaseModel)
+
+
+T = TypeVar('T')
+
+
+class BaseList(pydantic.RootModel[List[T]], Generic[T]):
+    root: List[T]
+
+    def __iter__(self):
+        return iter(self.root)
+
+    def __getitem__(self, item):
+        return self.root[item]
+
+    @classmethod
+    def doc_schema(cls):
+        return cls.model_json_schema(ref_template="#/components/schemas/{model}")
+
+    @classmethod
+    async def fetch(cls, db: Database, *args, **kwargs):
+        raise NotImplementedError("fetch() must be implemented in subclasses")
