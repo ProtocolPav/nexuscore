69ba5432f1ef76004cfb847074e07cb4dbda5760
Pavel <pbkrutov@gmail.com>
2025-05-03 17:01:06 +0300

    start working on userquests

--- src/models/users/quests.py
+++ src/models/users/quests.py
@@ -1,15 +1,119 @@
 from datetime import datetime
 from typing import Literal
 
-from pydantic import BaseModel, Field
+from pydantic import Field
 from typing_extensions import Optional
-
-import json
 
 from sanic_ext import openapi
 
 from src.database import Database
-from src.models.quests.objective import ObjectivesListModel
+from src.models.quests.objective import ObjectiveModel, ObjectivesListModel
+from src.utils.base import BaseModel, BaseList, optional_model
+from src.utils.errors import BadRequest400, NotFound404
+
+
+class UserQuestBaseModel(BaseModel):
+    thorny_id: int = Field(description="The ThornyID of a user. This is a unique number.",
+                           json_schema_extra={"example": 34})
+    quest_id: int = Field(description="The quest ID",
+                          json_schema_extra={"example": 453})
+
+
+class UserQuestModel(BaseModel):
+    accepted_on: datetime = Field(description="The time that the user accepted this quest",
+                                  json_schema_extra={"example": '2024-05-05 05:34:21.123456'})
+    started_on: Optional[datetime] = Field(description="The time the user actually started to complete the quest",
+                                           json_schema_extra={"example": '2024-05-05 05:34:21.123456'})
+    status: Literal['in_progress', 'completed', 'failed'] = Field(description="The status of the quest",
+                                                                  json_schema_extra={"example": 'in_progress'})
+    objectives: "UserObjectivesListModel" = Field(description="A list of all the objectives of this quest")
+
+    @classmethod
+    async def fetch(cls, db: Database, thorny_id: int = None, quest_id: int = None, *args) -> "UserQuestModel":
+        if not thorny_id and not quest_id:
+            raise BadRequest400('No IDs provided. Please provide a thorny and quest ID to fetch a quest by')
+
+        data = await db.pool.fetchrow("""
+                                      SELECT * from users.quests
+                                      WHERE thorny_id = $1
+                                      AND quest_id = $2
+                                      """,
+                                      thorny_id, quest_id)
+
+        objectives = await UserObjectiveModel.fetch_all_objectives(db, thorny_id, quest_id)
+
+        if data:
+            return cls(**data, objectives=objectives)
+        else:
+            raise NotFound404(extra={'resource': 'user_quest', 'id': f'{thorny_id}:{quest_id}'})
+
+    @classmethod
+    async def fetch_active_quest(cls, db: Database, thorny_id: int) -> Optional["UserQuestModel"]:
+        data = await db.pool.fetchrow("""
+                                      SELECT * from users.quests
+                                      WHERE thorny_id = $1
+                                      AND status = 'in_progress'
+                                      """,
+                                      thorny_id)
+
+        if data:
+            objectives = await UserObjectiveModel.fetch_all_objectives(db, thorny_id, data['quest_id'])
+
+            return cls(**data, objectives=objectives)
+
+        return None
+
+    @classmethod
+    async def get_all_quest_ids(cls, db: Database, thorny_id: int) -> Optional[list[int]]:
+        data = await db.pool.fetchrow("""
+                                      SELECT ARRAY_AGG(quest_id) AS quests from users.quests
+                                      WHERE thorny_id = $1
+                                      """,
+                                      thorny_id)
+
+        return data['quests'] if data else None
+
+    @classmethod
+    async def create(cls, db: Database, model: "UserQuestCreateModel", *args) -> int:
+        objectives_list = await ObjectivesListModel.fetch(db, model.quest_id)
+
+        async with db.pool.acquire() as conn:
+            async with conn.transaction():
+                await conn.execute("""
+                                   INSERT INTO users.quests(quest_id, thorny_id)
+                                   VALUES($1, $2)
+                                   """,
+                                   model.quest_id, model.thorny_id)
+
+                for objective in objectives_list:
+                    objective: ObjectiveModel
+
+                    await conn.execute("""
+                                       INSERT INTO users.objectives(quest_id, thorny_id, objective_id)
+                                       VALUES($1, $2, $3)
+                                       """,
+                                       model.quest_id, model.thorny_id, objective.objective_id)
+
+    async def mark_failed(self, db: Database, thorny_id: int):
+        self.status = 'failed'
+        await self.update(db, thorny_id)
+
+        for objective in self.objectives:
+            if objective.status == 'in_progress':
+                objective.status = 'failed'
+                await objective.update(db, thorny_id)
+
+    async def update(self, db: Database, thorny_id: int):
+        await db.pool.execute("""
+                               UPDATE users.quests
+                               SET accepted_on = $1,
+                                   started_on = $2,
+                                   status = $3
+                               WHERE thorny_id = $4
+                               AND quest_id = $5
+                               """,
+                              self.accepted_on, self.started_on,
+                              self.status, thorny_id, self.quest_id)
 
 
 @openapi.component()
@@ -67,97 +171,8 @@ class UserObjectiveModel(BaseModel):
         return cls.model_json_schema(ref_template="#/components/schemas/{model}")
 
 
-class UserQuestModel(BaseModel):
-    quest_id: int = Field(description="The quest ID", examples=[45])
-    accepted_on: datetime = Field(description="The time that the user accepted this quest",
-                                  examples=['2024-05-05 05:34:21.123456'])
-    started_on: Optional[datetime] = Field(description="The time the user actually started to complete the quest",
-                                           examples=['2024-03-21 12:33:45.123456'])
-    status: Literal['in_progress', 'completed', 'failed']
-    objectives: list[UserObjectiveModel]
-
-    @classmethod
-    async def fetch(cls, db: Database, thorny_id: int, quest_id: int) -> "UserQuestModel":
-        data = await db.pool.fetchrow("""
-                                      SELECT * from users.quests
-                                      WHERE thorny_id = $1
-                                      AND quest_id = $2
-                                      """,
-                                      thorny_id, quest_id)
-
-        objectives = await UserObjectiveModel.fetch_all_objectives(db, thorny_id, quest_id)
-
-        return cls(**data, objectives=objectives)
-
-    @classmethod
-    async def fetch_active_quest(cls, db: Database, thorny_id: int) -> Optional["UserQuestModel"]:
-        data = await db.pool.fetchrow("""
-                                      SELECT * from users.quests
-                                      WHERE thorny_id = $1
-                                      AND status = 'in_progress'
-                                      """,
-                                      thorny_id)
-
-        if data:
-            objectives = await UserObjectiveModel.fetch_all_objectives(db, thorny_id, data['quest_id'])
-
-            return cls(**data, objectives=objectives)
-
-        return None
-
-    @classmethod
-    async def get_all_quest_ids(cls, db: Database, thorny_id: int) -> Optional[list[int]]:
-        data = await db.pool.fetchrow("""
-                                      SELECT ARRAY_AGG(quest_id) AS quests from users.quests
-                                      WHERE thorny_id = $1
-                                      """,
-                                      thorny_id)
-
-        return data['quests'] if data else None
-
-    @classmethod
-    async def new(cls, db: Database, thorny_id: int, quest_id: int):
-        objectives_list = await ObjectivesListModel.fetch(db, quest_id)
-
-        async with db.pool.acquire() as conn:
-            async with conn.transaction():
-                await conn.execute("""
-                                   INSERT INTO users.quests(quest_id, thorny_id)
-                                   VALUES($1, $2)
-                                   """,
-                                   quest_id, thorny_id)
-
-                for objective in objectives_list:
-                    await conn.execute("""
-                                       INSERT INTO users.objectives(quest_id, thorny_id, objective_id)
-                                       VALUES($1, $2, $3)
-                                       """,
-                                       quest_id, thorny_id, objective.objective_id)
-
-    async def mark_failed(self, db: Database, thorny_id: int):
-        self.status = 'failed'
-        await self.update(db, thorny_id)
-
-        for objective in self.objectives:
-            if objective.status == 'in_progress':
-                objective.status = 'failed'
-                await objective.update(db, thorny_id)
-
-    async def update(self, db: Database, thorny_id: int):
-        await db.pool.execute("""
-                               UPDATE users.quests
-                               SET accepted_on = $1,
-                                   started_on = $2,
-                                   status = $3
-                               WHERE thorny_id = $4
-                               AND quest_id = $5
-                               """,
-                              self.accepted_on, self.started_on,
-                              self.status, thorny_id, self.quest_id)
-
-    @classmethod
-    def doc_schema(cls):
-        return cls.model_json_schema(ref_template="#/components/schemas/{model}")
+class UserQuestCreateModel(UserQuestBaseModel):
+    pass
 
 
 class UserQuestUpdateModel(BaseModel):
