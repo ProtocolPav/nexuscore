a44c23170a4951cdc1e4c60f4b7a8d7966d73d87
Pavel <pbkrutov@gmail.com>
2025-12-22 22:42:00 +0200

    refactor: use new quests_v3 tables for SQL

--- src/models/quests/objective_progress.py
+++ src/models/quests/objective_progress.py
@@ -1,14 +1,17 @@
+import json
 from datetime import datetime
 from typing import Literal
 
-from pydantic import Field
+from pydantic import Field, model_validator
 from typing_extensions import Optional
 
 from sanic_ext import openapi
 
 from src.database import Database
+from src.models.quests.objective_customization.customization import Customizations
 from src.models.quests.objective_customization.progress import CustomizationProgress
 from src.models.quests.objective_targets.progress import TargetProgress
+from src.models.quests.objective_targets.target import Targets
 from src.utils.base import BaseModel, BaseList, optional_model
 from src.utils.errors import BadRequest400, NotFound404
 
@@ -23,6 +26,17 @@ class ObjectiveProgressBaseModel(BaseModel):
     target_progress: list[TargetProgress] = Field(description="List of each objective target's progress")
     customization_progress: CustomizationProgress = Field(description="Specific customization info to track")
 
+    @model_validator(mode='before')
+    @classmethod
+    def pre_process_json(cls, data):
+        if isinstance(data.get('target_progress'), str):
+            data['target_progress'] = json.loads(data['target_progress'])
+
+        if isinstance(data.get('customization_progress'), str):
+            data['customization_progress'] = json.loads(data['customization_progress'])
+
+        return data
+
 
 @openapi.component()
 class ObjectiveProgressModel(ObjectiveProgressBaseModel):
@@ -32,63 +46,70 @@ class ObjectiveProgressModel(ObjectiveProgressBaseModel):
                               json_schema_extra={"example": 22})
 
     @classmethod
-    async def fetch(cls, db: Database, thorny_id: int = None, objective_id: int = None, *args) -> "ObjectiveProgressModel":
-        if not thorny_id and not objective_id:
-            raise BadRequest400(extra={'ids': ['thorny_id', 'objective_id']})
+    async def fetch(cls, db: Database, progress_id: int = None, objective_id: int = None, *args) -> "ObjectiveProgressModel":
+        if not progress_id or not objective_id:
+            raise BadRequest400(extra={'ids': ['progress_id', 'objective_id']})
 
         data = await db.pool.fetchrow("""
-                                      SELECT * from users.objectives
-                                      WHERE thorny_id = $1
+                                      SELECT * from quests_v3.objective_progress
+                                      WHERE progress_id = $1
                                         AND objective_id = $2
                                       """,
-                                      thorny_id, objective_id)
+                                      progress_id, objective_id)
 
         if data:
             return cls(**data)
         else:
-            raise NotFound404(extra={'resource': 'user_objective', 'id': f'{thorny_id}:{objective_id}'})
+            raise NotFound404(extra={'resource': 'objective_progress', 'id': f'Progress ID: {progress_id}, Objective ID:{objective_id}'})
 
     @classmethod
     async def create(cls, db: Database, model: "ObjectiveProgressCreateModel", *args):
         async with db.pool.acquire() as conn:
             async with conn.transaction():
+                targets = list(map(lambda x: x.model_dump(), model.target_progress))
+
                 await conn.execute("""
-                                   INSERT INTO users.objectives(quest_id, thorny_id, objective_id)
-                                   VALUES($1, $2, $3)
+                                   INSERT INTO quests_v3.objective_progress(
+                                       progress_id,
+                                       objective_id,
+                                       target_progress,
+                                       customization_progress
+                                   )
+                                   VALUES($1, $2, $3, $4)
                                    """,
-                                   model.quest_id, model.thorny_id, model.objective_id)
+                                   model.progress_id, model.objective_id,
+                                   json.dumps(targets, default=str), model.customization_progress.model_dump_json())
 
     async def update(self, db: Database, model: "ObjectiveProgressUpdateModel"):
         for k, v in model.model_dump().items():
             setattr(self, k, v) if v is not None else None
 
         await db.pool.execute("""
-                               UPDATE users.objectives
-                               SET "start" = $1,
-                                   "end" = $2,
-                                   "completion" = $3,
-                                   "status" = $4
-                               WHERE thorny_id = $5
-                               AND quest_id = $6
+                               UPDATE quests_v3.objective_progress
+                               SET start_time = $1,
+                                   end_time = $2,
+                                   target_progress = $3,
+                                   customization_progress = $4,
+                                   status = $5
+                               WHERE progress_id = $6
                                AND objective_id = $7
                                """,
-                              self.start, self.end, self.completion,
-                              self.status, self.thorny_id, self.quest_id, self.objective_id)
+                              self.start_time, self.end_time, self.target_progress, self.customization_progress,
+                              self.status, self.progress_id, self.objective_id)
 
 
 @openapi.component()
 class ObjectiveProgressListModel(BaseList[ObjectiveProgressModel]):
     @classmethod
-    async def fetch(cls, db: Database, thorny_id: int = None, quest_id: int = None, *args) -> "ObjectiveProgressListModel":
-        if not thorny_id and not quest_id:
-            raise BadRequest400(extra={'ids': ['thorny_id', 'quest_id']})
+    async def fetch(cls, db: Database, progress_id: int = None, *args) -> "ObjectiveProgressListModel":
+        if not progress_id:
+            raise BadRequest400(extra={'ids': ['progress_id']})
 
         data = await db.pool.fetch("""
-                                   SELECT * from users.objectives
-                                   WHERE thorny_id = $1
-                                     AND quest_id = $2
+                                   SELECT * from quests_v3.objective_progress
+                                   WHERE progress_id = $1
                                    """,
-                                   thorny_id, quest_id)
+                                   progress_id)
 
         objectives = []
         for objective in data:
@@ -98,12 +119,20 @@ class ObjectiveProgressListModel(BaseList[ObjectiveProgressModel]):
 
 
 class ObjectiveProgressCreateModel(BaseModel):
-    thorny_id: int = Field(description="The ThornyID of a user. This is a unique number.",
-                           json_schema_extra={"example": 34})
-    quest_id: int = Field(description="The quest ID",
-                          json_schema_extra={"example": 453})
+    progress_id: int = Field(description="The quest progress ID",
+                             json_schema_extra={"example": 453})
     objective_id: int = Field(description="The objective ID",
                               json_schema_extra={"example": 22})
+    target_progress: list[TargetProgress] = Field(description="List of each objective target's progress")
+    customization_progress: CustomizationProgress = Field(description="Specific customization info to track")
+
+    @classmethod
+    def generate_target_progress(cls, targets: list[Targets]):
+        ...
+
+    @classmethod
+    def generate_customization_progress(cls, customization: Customizations):
+        ...
 
 
 ObjectiveProgressUpdateModel = optional_model('ObjectiveProgressUpdateModel', ObjectiveProgressBaseModel)
