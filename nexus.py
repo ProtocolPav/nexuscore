44eec1bfd179955591209a64ee9edfdb6e55dd34
Pavel Krutov <pavel@krutov.eu>
2026-01-05 03:30:43 +0200

    fix: PATCH not being allowed on CORS

--- nexus.py
+++ nexus.py
@@ -14,7 +14,7 @@ app.config.SWAGGER_UI_CONFIGURATION = {"docExpansion": "list"}
 # Configure CORS
 app.config.CORS_ORIGINS = "http://localhost:3000,https://api.everthorn.net"  # Your TanStack Start app
 app.config.CORS_ALLOW_HEADERS = "*"
-app.config.CORS_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+app.config.CORS_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 app.config.CORS_SUPPORTS_CREDENTIALS = True  # For BetterAuth cookies
 app.config.CORS_AUTOMATIC_OPTIONS = True
 
