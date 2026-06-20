f44ea6e00e9958ca835a60c9e4ee9367cefd6bd4
Pavel Krutov <pavel@krutov.eu>
2026-06-20 18:52:37 +0300

    feat: add ObjectiveProgressRepository and update objective progress models for improved database interaction and type safety

--- src/models/quests/objective_progress.py
+++ src/models/quests/objective_progress.py
@@ -1,31 +1,51 @@
 import json
 from datetime import datetime
-from typing import Literal
+from typing import Annotated, Literal, Optional
 
-from pydantic import Field, model_validator
-from typing_extensions import Optional
-
-
-from src.dependencies.database import Database
-from asyncpg import Connection
+from pydantic import Field, model_validator, BaseModel
 
 from src.models.quests.objective_customization.customization import Customizations
 from src.models.quests.objective_customization.progress import CUSTOMIZATION_TYPE_MAP, CustomizationProgress
 from src.models.quests.objective_targets.progress import TARGET_TYPE_MAP, TargetProgress
 from src.models.quests.objective_targets.target import Targets
-from src.utils.base import BaseModel, BaseList, optional_model
-from fastapi import HTTPException
 
 
-class ObjectiveProgressBaseModel(BaseModel):
-    start_time: Optional[datetime] = Field(description="The time this objective was started on",
-                                           json_schema_extra={"example": '2024-05-05T05:34:21Z'})
-    end_time: Optional[datetime] = Field(description="The time that this objective was completed",
-                                         json_schema_extra={"example": '2024-05-05T05:34:21Z'})
-    status: Literal['active', 'pending', 'completed', 'failed'] = Field(description="The status of this objective",
-                                                                        json_schema_extra={"example": 'active'})
-    target_progress: list[TargetProgress] = Field(description="List of each objective target's progress")
-    customization_progress: CustomizationProgress = Field(description="Specific customization info to track")
+ProgressID = Annotated[int, Field(
+    description="The quest progress ID",
+    examples=[453]
+)]
+ObjectiveID = Annotated[int, Field(
+    description="The objective ID",
+    examples=[22]
+)]
+StartTime = Annotated[datetime, Field(
+    description="The time that this objective was started on",
+    examples=['2024-05-05T05:34:21Z']
+)]
+EndTime = Annotated[datetime, Field(
+    description="The time that this objective was completed",
+    examples=['2024-05-05T05:34:21Z']
+)]
+ProgressStatus = Annotated[Literal['active', 'pending', 'completed', 'failed'], Field(
+    description="The status of this objective",
+    examples=['active']
+)]
+TargetProgressList = Annotated[list[TargetProgress], Field(
+    description="List of each objective target's progress"
+)]
+CustomizationProgressDict = Annotated[CustomizationProgress, Field(
+    description="Specific customization info to track"
+)]
+
+
+class ObjectiveProgressDB(BaseModel):
+    progress_id: ProgressID
+    objective_id: ObjectiveID
+    start_time: Optional[StartTime]
+    end_time: Optional[EndTime]
+    status: ProgressStatus
+    target_progress: TargetProgressList
+    customization_progress: CustomizationProgressDict
 
     @model_validator(mode='before')
     @classmethod
@@ -39,86 +59,21 @@ class ObjectiveProgressBaseModel(BaseModel):
         return data
 
 
-class ObjectiveProgressModel(ObjectiveProgressBaseModel):
-    progress_id: int = Field(description="The quest progress ID",
-                             json_schema_extra={"example": 453})
-    objective_id: int = Field(description="The objective ID",
-                              json_schema_extra={"example": 22})
-
-    @classmethod
-    async def fetch(cls, db: Database, progress_id: int = None, objective_id: int = None, *args) -> "ObjectiveProgressModel":
-        if not progress_id or not objective_id:
-            raise HTTPException(status_code=400, detail="Missing required parameters")
-
-        data = await db.pool.fetchrow("""
-                                      SELECT * from quests_v3.objective_progress
-                                      WHERE progress_id = $1
-                                        AND objective_id = $2
-                                      """,
-                                      progress_id, objective_id)
-
-        if data:
-            return cls(**data)
-        else:
-            raise HTTPException(status_code=404, detail="Objective progress not found")
-
-    @classmethod
-    async def create(cls, db: Database, model: "ObjectiveProgressCreateModel", conn: Connection = None, *args):
-        if conn is not None:
-            async with conn.transaction():
-                targets = list(map(lambda x: x.model_dump(), model.target_progress))
-
-                await conn.execute("""
-                                   INSERT INTO quests_v3.objective_progress(
-                                       progress_id,
-                                       objective_id,
-                                       target_progress,
-                                       customization_progress
-                                   )
-                                   VALUES($1, $2, $3, $4)
-                                   """,
-                                   model.progress_id, model.objective_id,
-                                   json.dumps(targets, default=str), model.customization_progress.model_dump_json())
-
-    async def update(self, db: Database, model: "ObjectiveProgressUpdateModel"):
-        for k in model.model_dump().keys():
-            v = getattr(model, k)
-            setattr(self, k, v) if v is not None else None
-
-        targets = list(map(lambda x: x.model_dump(), self.target_progress))
-
-        await db.pool.execute("""
-                               UPDATE quests_v3.objective_progress
-                               SET start_time = $1,
-                                   end_time = $2,
-                                   target_progress = $3,
-                                   customization_progress = $4,
-                                   status = $5
-                               WHERE progress_id = $6
-                               AND objective_id = $7
-                               """,
-                              self.start_time, self.end_time,
-                              json.dumps(targets, default=str), self.customization_progress.model_dump_json(),
-                              self.status, self.progress_id, self.objective_id)
+class ObjectiveProgressOut(ObjectiveProgressDB):
+    pass
 
 
-class ObjectiveProgressListModel(BaseList[ObjectiveProgressModel]):
-    @classmethod
-    async def fetch(cls, db: Database, progress_id: int = None, *args) -> "ObjectiveProgressListModel":
-        if not progress_id:
-            raise HTTPException(status_code=400, detail="Missing required parameters")
+class ObjectiveProgressIn(BaseModel):
+    target_progress: TargetProgressList
+    customization_progress: CustomizationProgressDict
 
-        data = await db.pool.fetch("""
-                                   SELECT * from quests_v3.objective_progress
-                                   WHERE progress_id = $1
-                                   """,
-                                   progress_id)
 
-        objectives = []
-        for objective in data:
-            objectives.append(ObjectiveProgressModel(**objective))
-
-        return cls(root=objectives)
+class ObjectiveProgressUpdate(BaseModel):
+    start_time: Optional[StartTime] = None
+    end_time: Optional[EndTime] = None
+    status: Optional[ProgressStatus] = None
+    target_progress: Optional[TargetProgressList] = None
+    customization_progress: Optional[CustomizationProgressDict] = None
 
 
 class ObjectiveProgressCreateModel(BaseModel):
@@ -150,6 +105,3 @@ class ObjectiveProgressCreateModel(BaseModel):
                 customization_progress[customization] = customization_model()
 
         return CustomizationProgress(**customization_progress)
-
-
-ObjectiveProgressUpdateModel = optional_model('ObjectiveProgressUpdateModel', ObjectiveProgressBaseModel)
