0ff34477624808ef25b935e55fac9577f2a4e133
Pavel Krutov <pavel@krutov.eu>
2026-08-08 00:30:29 +0100

    feat: switch to OpenTelemetry HTTP exporter and update dependencies

--- src/telemetry.py
+++ src/telemetry.py
@@ -2,7 +2,7 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.resources import Resource
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace.sampling import ParentBased, TraceIdRatioBased
 
 from settings import settings
